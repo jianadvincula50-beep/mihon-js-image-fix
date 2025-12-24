15160bb02a6713c2291cef3b1114623f718b30b7
jianadvincula50-beep <jianadvincula50@gmail.com>
2025-12-24 14:14:08 +0800

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){if(!window.location.hostname.includes("mihon"))return;setInterval(()=>{document.querySelectorAll('img').forEach(img=>{if(img.src.includes('//k')&&img.src.includes('.mb')){img.referrerPolicy='no-referrer';img.src=img.src.replace('//k','//n')}})},2000)})();
